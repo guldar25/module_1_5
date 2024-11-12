428db4f97e22261879eedc071a36f4f289197681
guldar25 <yasybaevag@gmail.com>
2024-11-12 17:10:59 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, "water", 4.1
+print(immutable_var)
+# immutable_var[0] = 2 кортеж является неизменяемым списком
+mutable_list = [3, 6.1, "juice"]
+print(mutable_list)
+mutable_list[0] = 12
+print(mutable_list)
